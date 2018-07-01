b9110764e484eee5539c9fb6949d1500825361af
Adib Shalekeen <ashaleke@edu.uwaterloo.ca>
2018-07-01 05:30:18 -0400

    working add.x

--- pd3/alu_input_ctrlr.v
+++ pd3/alu_input_ctrlr.v
@@ -8,7 +8,7 @@ module alu_input_ctrlr(
 input wire w_mem_op, w_imm_op, w_shift_op;
 output reg w_alu_lhs_ctrl;
 output reg [1:0] w_alu_rhs_ctrl;
-always @(w_imm_op, w_shift_op)
+always @(*)
     begin
         if(w_shift_op)
             begin
